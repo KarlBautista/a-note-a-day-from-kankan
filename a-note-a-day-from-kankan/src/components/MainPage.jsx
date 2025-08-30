60f1444cc4c0d151095fe72fbc2c71f339b67a71
Karl <karlbautista234@gmail.com>
2025-08-31 02:06:39 +0800

    finished all the designs

--- a-note-a-day-from-kankan/src/components/MainPage.jsx
+++ a-note-a-day-from-kankan/src/components/MainPage.jsx
@@ -7,6 +7,8 @@ const MainPage = () => {
         <h1>♡ A Note a Day From Kankan ♡</h1>
         <h2>Because every day with you ganing is special</h2>
         <NoteCard />
+
+         <footer>♡ Made with love for the love of my life ♡</footer>
       
     </div>
   )
--- a-note-a-day-from-kankan/src/components/NoteCard.jsx
+++ a-note-a-day-from-kankan/src/components/NoteCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "../styles/NoteCard.css"
+import Date from "../assets/date.png"
 const NoteCard = () => {
   return (
     <div className='note-card'>
-       <div className="date">08/31/2025 <span style={{"float":"right" }}>With love ❤️</span></div>
+        <div className="date-container">
+          <div className="date">
+          
+            <div className='date-text'><img src={Date} alt="" />  08/31/2025</div> 
+            <div style={{ "float": "right"}}>With love ❤️</div>
+          </div>
+        </div>
+     
     <p>
       My dearest, today I want to remind you how much you mean to me. Your smile brightens even my darkest days, and your love gives me strength I never knew I had. Forever yours, Kankan
     </p>
